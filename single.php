355994b23fb3226da0d362128c49586c72db599e
Daniele Scasciafratte <mte90net@gmail.com>
2019-02-22 16:45:15 +0100

    working on menu

--- single.php
+++ single.php
@@ -48,4 +48,24 @@ $container = get_theme_mod( 'understrap_container_type' );
 
 </div><!-- #single-wrapper -->
 
+<nav class="navbar navbar-expand-sm navbar-dark navbar-right bg-info">
+	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-abel="<?php esc_attr_e( 'Toggle navigation', 'understrap' ); ?>">
+		<span class="navbar-toggler-icon"></span>
+	</button>
+
+	<!-- The WordPress Menu goes here -->
+	<?php wp_nav_menu(
+		array(
+			'theme_location'  => 'social',
+			'container_class' => 'collapse navbar-collapse',
+			'container_id'    => 'navbarNavDropdown',
+			'menu_class'      => 'navbar-nav ml-auto',
+			'fallback_cb'     => '',
+			'menu_id'         => 'main-menu',
+			'depth'           => 2,
+			'walker'          => new Understrap_WP_Bootstrap_Navwalker(),
+		)
+	); ?>
+</nav><!-- .site-navigation -->
+
 <?php get_footer(); ?>
