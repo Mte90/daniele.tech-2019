9fa516a158bb41a7e97b527b1bc2a0b83f346363
Daniele Scasciafratte <mte90net@gmail.com>
2019-03-01 19:40:01 +0100

    working on the home page and improvements

--- inc/extras.php
+++ inc/extras.php
@@ -152,7 +152,7 @@ function get_last_5_from_cat( $id ) {
 	$out      = '<h3>' . get_cat_name( $pl_id ) . '</h3>';
 	$out     .= '<ul>';
 	$args     = array(
-		'cat'              => $id . ', -272',
+		'cat'              => $id,
 		'posts_per_page'   => 5,
 		'order'            => 'DESC',
 		'orderby'          => 'date',
