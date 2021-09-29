187d7892c8e36165a5ca796e84b0078efa8050f8
Daniele Scasciafratte <mte90net@gmail.com>
2021-09-29 12:17:22 +0200

    fix(gitlab): exclude private stuff

--- inc/extras.php
+++ inc/extras.php
@@ -267,7 +267,7 @@ function gh_month_pr( $atts ){
 
 	$output = $output_gh;
 
-	if ( false === ( $output_gl = get_transient( 'gitlab_month_status_'. $a[ 'month' ] . '_' . $a[ 'year' ] ) ) ) {
+// 	if ( false === ( $output_gl = get_transient( 'gitlab_month_status_'. $a[ 'month' ] . '_' . $a[ 'year' ] ) ) ) {
 		$a[ 'date_end' ] = date("Y-m-t", strtotime($dateString));
 		$url = "https://gitlab.com/api/v4/merge_requests?author_username=Mte90&created_after=" . $a[ 'date_before' ] . "T08:00:00Z%20&created_before=" . $a[ 'date_end' ] . "T24:00:00Z";
 		$response = wp_remote_get( $url,array(
@@ -280,9 +280,13 @@ function gh_month_pr( $atts ){
 		$output_gl = '<h3>GitLab</h3><ul>';
 
 		foreach ($repos as $repo) {
+			$name = explode('/', $repo->web_url);
+			if ( $name[3] === 'codeat' ) {
+				continue;
+			}
+
 			$output_gl .= '<li>';
 			$output_gl .= '<a href="' . $repo->web_url . '" target="_blank">';
-			$name = explode('/', $repo->web_url);
 			$output_gl .= $name[3] . '/' . $name[4] . ' - ' . $repo->title;
 			$output_gl .= '</a>';
 			$output_gl .= '</li>';
@@ -295,18 +299,23 @@ function gh_month_pr( $atts ){
 				'Authorization' => 'Bearer ' . GITLAB_TOKEN,
 			),
 		) );
-		$repos = json_decode( wp_remote_retrieve_body( $response ) );
+		$issues = json_decode( wp_remote_retrieve_body( $response ) );
 		$closed = $open = 0;
-		foreach ($repos as $repo) {
-            if( $repo->state === 'closed' ) {
+		foreach ($issues as $issue) {
+			$name = explode('/', $issue->web_url);
+			if ( $name[3] === 'codeat' ) {
+				continue;
+			}
+
+            if( $issue->state === 'closed' ) {
                 $closed++;
-            } else if( $repo->state === 'opened' ) {
+            } else if( $issue->state === 'opened' ) {
                 $open++;
             }
 		}
 		$output_gl .= '<a href="https://gitlab.com/dashboard/issues/?author_username=Mte90" target="_blank">This month on GitLab I opened ' . $open . ' tickets and closed ' . $closed . '.</a><br><br>';
 		set_transient( 'gitlab_month_status_' . $a[ 'month' ] . '_' . $a[ 'year' ], $output_gl, WEEK_IN_SECONDS);
-	}
+// 	}
 
 	$output .= $output_gl;
 
