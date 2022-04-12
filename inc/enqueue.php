c1c0dc444edef59de4190b94754d929756e18386
Daniele Scasciafratte <mte90net@gmail.com>
2022-04-12 17:23:38 +0200

    feat(css): mobile menu

--- inc/enqueue.php
+++ inc/enqueue.php
@@ -29,7 +29,7 @@ if ( ! function_exists( 'understrap_scripts' ) ) {
 		wp_enqueue_style( 'fontawesome', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.7.2/css/fontawesome.min.css', array(), $css_version );
 
 		$js_version = $theme_version . '.' . filemtime( get_template_directory() . '/js/theme.js' );
-		wp_enqueue_script( 'understrap-scripts', get_template_directory_uri() . '/js/theme.js', array(), $js_version, true );
+		wp_enqueue_script( 'understrap-scripts', get_template_directory_uri() . '/js/theme.js', array('jquery'), $js_version, true );
 		if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 			wp_enqueue_script( 'comment-reply' );
 		}
