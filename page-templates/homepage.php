c7334e71572baad5ccc32ef0d11748037769dd87
Daniele Scasciafratte <mte90net@gmail.com>
2022-09-02 11:54:05 +0200

    Update homepage.php

--- page-templates/homepage.php
+++ page-templates/homepage.php
@@ -28,7 +28,7 @@ $container = get_theme_mod( 'understrap_container_type' );
 						<div class="row">
 							<div class="col-md-6">
 								<p>
-                                    <a href="https://daniele.tech/2020/07/contribute-to-open-source-the-right-way-2nd-edition-download-the-free-open-book-now"><img class="wp-image-4261 size-medium aligncenter" src="https://daniele.tech/wp-content/uploads/2020/07/cover-300x279.png" alt="" width="300" height="279" /></a>
+                                    <a href="https://daniele.tech/2022/09/contribute-to-open-source-the-right-way-3nd-edition/"><img class="wp-image-4261 size-medium aligncenter" src="https://daniele.tech/wp-content/uploads/2022/09/cover-2022-1-300x300.png" alt="" width="300" height="300" /></a>
 								</p>
 							</div>
 							<div class="col-md-6">
