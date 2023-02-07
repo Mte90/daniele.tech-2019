584be68eafe507e258039f27a364c4b24c66bcc8
Daniele Scasciafratte <mte90net@gmail.com>
2023-02-07 15:14:37 +0100

    feat(ga): v4

--- header.php
+++ header.php
@@ -20,13 +20,22 @@ $container = get_theme_mod( 'understrap_container_type' );
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<?php wp_head(); ?>
+
+	<!-- Google tag (gtag.js) -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=G-M1X9XPS3F8"></script>
+	<script>
+	window.dataLayer = window.dataLayer || [];
+	function gtag(){dataLayer.push(arguments);}
+	gtag('js', new Date());
+
+	gtag('config', 'G-M1X9XPS3F8');
+	</script>
 </head>
 
 <body <?php body_class(); ?>>
 
 <div class="site" id="page">
 
-	<!-- ******************* The Navbar Area ******************* -->
 	<div id="wrapper-navbar" itemscope itemtype="http://schema.org/WebSite">
 
 		<a class="skip-link sr-only sr-only-focusable" href="#content"><?php esc_html_e( 'Skip to content', 'understrap' ); ?></a>
@@ -40,12 +49,12 @@ $container = get_theme_mod( 'understrap_container_type' );
 		<a class="navbar-brand" rel="home" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" itemprop="url"><?php bloginfo( 'name' ); ?></a> The Open Source Multiversal Guy
 
 		<?php if ( 'container' === $container ) : ?>
-			</div><!-- .container -->
+			</div>
 		<?php endif; ?>
 
-		</nav><!-- .site-navigation -->
+		</nav>
 
-	</div><!-- #wrapper-navbar end -->
+	</div>
 
 	<nav class="navbar navbar-expand-sm navbar-dark navbar-left bg-info d-md-block">
 		<button class="navbar-toggler d-lg-none" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="<?php esc_attr_e( 'Toggle navigation', 'understrap' ); ?>">
